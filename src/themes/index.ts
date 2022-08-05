32fc95370394db5790ae49e535d1d96c767d3498
Mike <mikelrap@gmail.com>
2022-08-05 15:35:42 +0300

    primary color returned

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -2,13 +2,14 @@ import { Theme } from 'theme-ui';
 import darkTheme from './dark';
 import lightTheme from './light';
 
-export const getTheme = (): Theme => {
+export const getTheme = (primaryColor: string): Theme => {
   return {
     config: {
       initialColorModeName: 'light',
     },
     colors: {
       ...lightTheme,
+      primaryColor: primaryColor,
       modes: {
         dark: darkTheme,
       },
