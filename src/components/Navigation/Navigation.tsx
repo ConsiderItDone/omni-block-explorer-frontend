dbfb8164beb6c51ba85b9380304fcc2f8007a765
ramil <ramilexe@gmail.com>
2022-09-21 13:23:37 +0300

    get rid of logo

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -65,7 +65,7 @@ export default React.memo(() => {
         </h3>
         {navigation}
       </div>
-      <img className="bottom_logo" src={logoConsider} />
+      {/*<img className="bottom_logo" src={logoConsider} />*/}
     </nav>
   );
 });
