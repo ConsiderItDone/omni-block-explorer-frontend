347e0ed97501e118c79a44c0dab57bd1ef16e11e
Brioline <mikelrap@gmail.com>
2022-09-21 12:56:08 +0300

    primary color fix

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -1,5 +1,4 @@
 export default {
-  primary: '#F40079',
   background: '#071423',
   'window-dark': '#0F1C2A',
   gray: '#878D94',
--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,5 +1,4 @@
 export default {
-  primary: '#F40079',
   background: 'white',
   'window-dark': '#F7F7F7',
   gray: '#A5A5A5',
