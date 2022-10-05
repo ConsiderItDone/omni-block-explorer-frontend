3150c131d975220f516d5933a1b5d49a27416b1b
Mike <mikelrap@gmail.com>
2022-10-05 11:23:40 +0300

    updated mounting to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
@@ -7,7 +8,10 @@ import { default as apolloClient } from './api/client';
 import App from './App';
 import './styles/index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <ApolloProvider client={apolloClient}>
     <BrowserRouter>
       <Routes>
@@ -15,5 +19,4 @@ ReactDOM.render(
       </Routes>
     </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById('root'),
 );
